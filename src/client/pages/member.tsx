dacc668e83a44408b6213f41c8dff9e0653f2b9a
Dusadee <dusadee.srj@gmail.com>
2021-09-06 13:49:51 +0700

    apdate member list

--- src/client/pages/member.tsx
+++ src/client/pages/member.tsx
@@ -6,7 +6,7 @@ const Member: NextPage = () => {
   return (
     <LayoutTemplate title="Member List">
       <main className={styles.main}>
-        <h1 className={styles.title}>Member List (Round1)</h1>
+        <h1 className={styles.title}>Member List (Final!)</h1>
       </main>
       <Container>
         <ul>
@@ -15,9 +15,11 @@ const Member: NextPage = () => {
           <li>Pa</li>
           <li>Mookky</li>
           <li>Mean</li>
+          <li>Kie</li>
           <li>Chaii</li>
           <li>Now</li>
           <li>Taeto</li>
+          <li>Boom</li>
         </ul>
       </Container>
     </LayoutTemplate>
